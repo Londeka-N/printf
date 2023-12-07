eab3270eec267575514542eaa23258c945766067
Londeka-N <londeka.immaculaten@gmail.com>
2023-12-07 12:49:53 -0800

    done

---
+++ printf.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+* _printf - personal print function
+* @format: the string to be printed
+*
+* Return: the lengthof string
+*/
+
+int _printf(const char *format, ...)
+{
+	int i = 0, printedlength = 0;
+	va_list arguments;
+
+	print_t prints[] = {
+		{"%s", printf_str}, {"%%", printf_37}, {"%r", printf_revstr},
+		{"%c", printf_c}, {"%d", printf_int}, {"%i", printf_int},
+		{"%b", printf_b}, {"%R", printf_rot}, {"%p", printf_ptr}};
+	va_start(arguments, format);
+
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	while (format[i] != '\0')
+	{
+		unsigned long int j = 0;
+		int specifierfound = 0;
+
+		while (j < (sizeof(prints) / sizeof(print_t)))
+		{
+			if (prints[j].specifier[0] == format[i]
+					prints[j].specifier[1] == format[i + 1])
+			{
+				printedlength += prints[j].print_function(arguments);
+				specifierfound = 1;
+				i += 2;
+				break;
+			}
+			j++;
+		}
+		if (!specifierfound)
+		{
+			_putchar(format[i]);
+			printedlength++;
+			i++;
+		}
+	}
+	va_end(arguments);
+	return (printedlength);
+}
